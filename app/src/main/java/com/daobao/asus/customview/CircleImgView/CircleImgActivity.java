d5dac592a49b9c74fb40a46620750f3f6b065b72
dabao <1006112544@qq.com>
2018-05-22 12:19:43 +0800

    更新CircleImageView

---
+++ app/src/main/java/com/daobao/asus/customview/CircleImgView/CircleImgActivity.java
@@ -0,0 +1,19 @@
+package com.daobao.asus.customview.CircleImgView;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.daobao.asus.customview.R;
+
+/**
+ * Created by db on 2018/5/22.
+ */
+
+public class CircleImgActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_circle_img);
+    }
+}
